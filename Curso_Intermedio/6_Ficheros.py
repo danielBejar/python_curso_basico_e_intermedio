7c8abcd60a6f9431d310b46567ae2c49b2348b17
danielBejar <danielbejar42@gmail.com>
2023-10-20 03:56:00 -0300

    ficheros

--- Curso_Intermedio/6_Ficheros.py
+++ Curso_Intermedio/6_Ficheros.py
@@ -1,9 +1,10 @@
+print("1--------------------------------------------------------------------------------")
 #ficheros de archivos TXT
 
 #Para abrir un fichero utilizamos la operacion Open
 
 entrada = open("fichero.txt","r")  #El r es para poder leerlo, si queremos escribir usariamos el comando w
-                                    #Para leer y escribir ponemos w+
+                                    #Para escribir ponemos "w" o "a"
 print(entrada.read())              #lee el txt de forma completa, si no se ejecuta nada, hay que entrar desde la terminal a la carpeta donde esta el archivo.py C:\Users\danielsan\Dropbox\VSCODE\python\Curso_Intermedio> (Verificar que la terminal este en la carpeta correcta)
 
 #print(entrada.read (10))           #Solo imprime los primeros 10 caracerere (comentar el print anterior para que funcione)
@@ -25,14 +26,47 @@ print(entrada.read())              #lee el txt de forma completa, si no se ejecu
 #print("vector vacio:",entrada.readlines()) #Este comando no mostraria nada, debido a que el primer entrada.read() dejo el puntero al final del txt.
 
 
-#Para escribir debemos usar el comendo w al cargar
-salida= open("fichero2.txt","w") 
-salida.write("\n Texto agregadoooo") #El \n es para que escriba en la linea siguiente y no pegado a la ultima linea
+#Para escribir debemos usar el comando cargar w o a.
+salida= open("fichero.txt","a") #El valor a , hace que lo que agreguemos se inserte al final
+salida.write("\nTexto agregadoooo") #El \n es para que escriba en la linea siguiente y no pegado a la ultima linea
 
-#Aclaracion, si no existe el archivo fichero2.txt el VA A CREARLO.
+#Aclaracion, si no existe el archivo fichero.txt el VA A CREARLO.
+salida2= open("fichero2.txt","w") #El comando w, lo que hace es borrar todo lo que haya dentro de fichero2.txt antes de escribir en el
+salida2.write("Texto agregadoooo")
+
+
 
 #Cuando usamos la funcion open lo que hacemos es abrir el fichero desde python, lo que se debe hacer al finalizar de
 #leer o escribir en un fichero es cerrarlo. 
-
 entrada.close() #Cerramos los ficheros usados
 salida.close()
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------
+print("2 --------------------------------------------------------------------------------")
+
+#Utilizando el comando with
+#La sentencia with. La sentencia with se usa para ajustar la ejecución de un bloque con métodos definidos por un administrador de contexto
+#Esta sentencia se ejecuta y se cierra cuando se finaliza el with, de esta manera no es necesario utilizar los comandos .close()
+
+with open("fichero.txt", "r") as archivo:
+    print(archivo.read()) 
+
+#no es necesario utilizar el close, debido a que se cierra el archivo cuando se termina de ejecutar el bloque with
+
+print("3 --------------------------------------------------------------------------------")
+
+#Otra forma de imprimir un txt es recorriendo linea a linea con un for
+
+with open("fichero.txt", "r") as archivo:
+    lineas = archivo.read()
+    for l in lineas:
+        print (l, end= "")
+
+print()
+print("4 --------------------------------------------------------------------------------")
+
+#Tambien podemos saber la posicion donde estamos dentro del txt, para ello usamos el comando tell
+
+with open("fichero.txt", "r") as archivo:
+    lineas = archivo.read()
+    print(archivo.tell()) #No da el valor de la posicion donde estamos
